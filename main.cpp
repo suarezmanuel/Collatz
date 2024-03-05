917b789946cc4765592b40ca8be7bc4a07e99bb5
suarezmanuel <manuelsuarez244@gmail.com>
2024-03-05 12:19:08 +0200

    naive+

--- main.cpp
+++ main.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 
-// next step: use memoization to speed up the proccess
-int collaz (int num, int count=1) {
-    if (num < 1) { return -1; }
+int calc (int num, int count) {
     if (num == 1) { return count; }
-    if (num % 2 == 0) { collaz (num/2, count++); }
-    else { collaz (3*num + 1, count++); }
+    if (num % 2 == 0) { return calc (num/2, ++count); }
+    else { return calc (3*num + 1, ++count); }
+}
+
+int collaz (int num, int count=0) {
+    if (num < 1) { return -1; }
+    return calc (num, count);
 }
 
 int main () {
-    std::cout << collaz(8) << std::endl;
+    std::cout << "enter your number bigger than 0" << std::endl;
+    int n = 0; std::cin >> n;
+    std::cout << "collaz(" << n << ")" << " takes " << collaz(n) << " iterations" << std::endl;
 }
